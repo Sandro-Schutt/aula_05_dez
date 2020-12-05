f06a3119939b9204466370f13162b8b0d3101982
schut <schut@DESKTOP-12C8RFD>
2020-12-05 11:44:29 -0300

    Bootcamp Fábrica de Dev - aula de 05_12

---
+++ exercicio002/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace exercicio002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Entre com um valor numerico: ");
+            var valorDigitadoEhInteiro = int.TryParse(Console.ReadLine(), out int valorDigitado);
+            Console.WriteLine("O Valor Digitado está certo?:{0} foi: {1}", valorDigitadoEhInteiro, valorDigitado);
+
+            if (valorDigitadoEhInteiro)
+            {
+                Console.WriteLine("Válido ", valorDigitadoEhInteiro);
+            }
+            else
+            {
+                Console.WriteLine("Inválido ", valorDigitadoEhInteiro);
+            }
+
+        }
+    }
+}
---
+++ exercicio02/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace exercicio02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
